7f112a96900947cabaa4ea2bd28ff42a1467752a
Richie Casto <rcasto92@gmail.com>
2021-06-19 13:30:57 -0400

    peers and config start

--- test/index.js
+++ test/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const peerConfig = {
+    iceServers: [
+        // Google STUN Servers
+        {
+            urls: [
+                'stun.l.google.com:19302',
+                'stun1.l.google.com:19302',
+                'stun2.l.google.com:19302',
+            ],
+        }
+    ],
+};
+
+const peer1 = new RTCPeerConnection(peerConfig);
+const peer2 = new RTCPeerConnection(peerConfig);
