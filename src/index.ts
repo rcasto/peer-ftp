ed7b055050167d61def565f62c534e1ce00e0f8b
Richie Casto <rcasto92@gmail.com>
2021-07-04 09:23:42 -0400

    we streaming some data now, it works kind of at least

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ import Peer from 'simple-peer';
 import prompt from 'prompt';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import { access, createReadStream, createWriteStream } from 'fs';
+import { promisify } from 'util';
+
+const accessPromise = promisify(access);
 
 // simple-peer internally supplies default ice servers:
 // https://github.com/feross/simple-peer/blob/d972548299a50f836ca91c36e39304ef0f9474b7/index.js#L1038
 
-async function host() {
+async function host(inputFilePath: string): Promise<void> {
+    // Check if input file path exists
+    await accessPromise(inputFilePath);
+
     const hostPeer = new Peer({
         initiator: true,
         trickle: false,
@@ -16,7 +23,7 @@ async function host() {
     });
 
     hostPeer.on('signal', async data => {
-        console.log(`Send the below offer session description to your peer:\n${JSON.stringify(data)}`);
+        console.log(`Send the below offer session description to your peer:\n\n${JSON.stringify(data)}\n`);
 
         const { answer } = await prompt.get([
             'answer',
@@ -24,44 +31,76 @@ async function host() {
 
         hostPeer.signal(answer as string);
     });
-
     hostPeer.on('connect', () => {
         console.log('Connected to peer!');
 
-        hostPeer.send('ping');
-    });
+        const inputFileReadableStream = createReadStream(inputFilePath, {
+            encoding: 'utf8',
+        });
 
-    hostPeer.on('data', data => {
-        console.log(data);
-    });
+        inputFileReadableStream.on('data', chunk => {
+            hostPeer.send(JSON.stringify({
+                type: 'data',
+                data: chunk,
+            }));
+        });
 
+        inputFileReadableStream.on('end', () => {
+            console.log('Done transmitting file to peer.');
+
+            // Let peer know the end has been reached
+            hostPeer.send(JSON.stringify({
+                type: 'end',
+            }));
+        });
+
+        inputFileReadableStream.on('error', err => {
+            console.error(`Error occurred during file transfer: ${err}`);
+        });
+    });
     hostPeer.on('error', err => {
         console.error(`Error: ${err}`);
     });
 }
 
-async function client() {
+async function client(outputFilePath: string): Promise<void> {
     const clientPeer = new Peer({
         initiator: false,
         trickle: false,
         objectMode: true,
         wrtc,
     });
-
-    clientPeer.on('signal', async data => {
-        console.log(`Client Signal:\n${JSON.stringify(data)}`);
+    const outputFileWritableStream = createWriteStream(outputFilePath, {
+        encoding: 'utf8',
     });
 
+    outputFileWritableStream.on('finish', () => {
+        console.log("Write completed.");
+    });
+    outputFileWritableStream.on('error', err => {
+        console.error(`Error occurred during file transfer: ${err}`);
+    });
+
+    clientPeer.on('signal', data => {
+        console.log(`\nSend the below answer session description back to your peer:\n\n${JSON.stringify(data)}\n`);
+    });
     clientPeer.on('connect', () => {
         console.log('Connected to peer!');
     });
+    clientPeer.on('data', message => {
+        const { type, data } = JSON.parse(message);
 
-    clientPeer.on('data', data => {
-        console.log(data);
-
-        clientPeer.send('pong');
+        switch(type) {
+            case 'data':
+                outputFileWritableStream.write(data);
+                break;
+            case 'end':
+                outputFileWritableStream.end();
+                break;
+            default:
+                console.log(`Unknown message type ${type} received: ${message}`);
+        }
     });
-
     clientPeer.on('error', err => {
         console.error(`Error: ${err}`);
     });
@@ -75,7 +114,7 @@ async function client() {
 
 // npx peer-cli -i <input-file-path>   ---> offerer
 // npx peer-cli -o <output-file-path>  ---> answerer
-async function main() {
+async function main(): Promise<void> {
     const argv = await yargs(hideBin(process.argv))
         .option('input', {
             alias: 'i',
@@ -98,10 +137,13 @@ async function main() {
         })
         .argv;
 
-    if (argv.input) {
-        client();
+    const filePath = argv.input || argv.output || '';
+    const isInputFile = !!argv.input;
+
+    if (isInputFile) {
+        await host(filePath);
     } else {
-        host();
+        await client(filePath);
     }
 }
 
